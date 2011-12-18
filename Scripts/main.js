94701c014df4cb628c1ebae13d41482f8f129cf1
Genero Generus <g3n3r0.g3n3ru5@gmail.com>
2011-12-18 20:14:31 +0000

    Worked out way to get around resource loading problems.

--- Scripts/main.js
+++ Scripts/main.js
@@ -42,32 +42,50 @@ window.onload = function() {
         };
     }
     function genChar(head,body,arms,legs,elem,name) {
+        //var rsrcLength = 2;
         //alert("Durr");
         var canvas = document.createElement("canvas");
         canvas.width = 128;
         canvas.height = 256;
         var c = canvas.getContext("2d");
         //var headCols = ["#F00", "#0F0", "#00F"];
-        var headImgs = ["Graphics/blue_1_128.png", "Graphics/red_1_128.png", "Graphics/green_1_128.png", "Graphics/yellow_1_128.png"];
-        var bodyCols = ["#F00", "#0F0", "#00F"];
+        var headImgs = ["Graphics/Body/Head/blue_1_128.png", "Graphics/Body/Head/red_1_128.png", "Graphics/Body/Head/green_1_128.png", "Graphics/Body/Head/yellow_1_128.png"];
+        var headIds = ["head-blue_1_128", "head-red_1_128", "head-green_1_128", "head-yellow_1_128"];
+        //var bodyCols = ["#F00", "#0F0", "#00F"];
+        var bodyImgs = ["Graphics/Body/Torso/suit.png", "Graphics/Body/Torso/dragonBody.png", "Graphics/Body/Torso/creeperBody.jpg"];
         var armsCols = ["#F00", "#0F0", "#00F"];
         var legsCols = ["#F00", "#0F0", "#00F", "#F0F"];
         //var h = headCols[head];
-        var h = headImgs[head];
-        var b = bodyCols[body];
+        //var h = headImgs[head];
+        var h = headIds[head];
+        //var b = bodyCols[body];
+        var b = bodyImgs[body];
         var a = armsCols[arms];
         var l = legsCols[legs];
         
         //c.fillStyle = h;
         //c.fillRect(0.25*canvas.width,0,canvas.width/2,canvas.height/4);
-        var hImg = new Image();
+        /*var hImg = new Image();
         hImg.onload = function() {
             c.drawImage(this, 0.25*canvas.width,0,canvas.width/2,canvas.height/4);
+            var bImg = new Image();
+            bImg.onload = function() {
+                c.drawImage(this, 0.25*canvas.width,0.25*canvas.height,canvas.width/2,canvas.height/2);
+                return canvas.toDataURL("image/png");
+            };
+            bImg.src = b;
         };
-        hImg.src = h;
+        hImg.src = h;*/
+        var hImg = document.getElementById(h);
+        c.drawImage(hImg, 0.25*canvas.width,0,canvas.width/2,canvas.height/4);
         
-        c.fillStyle = b;
-        c.fillRect(0.25*canvas.width,0.25*canvas.height,canvas.width/2,canvas.height/2);
+        //c.fillStyle = b;
+        //c.fillRect(0.25*canvas.width,0.25*canvas.height,canvas.width/2,canvas.height/2);
+        var bImg = new Image();
+        bImg.onload = function() {
+            c.drawImage(this, 0.25*canvas.width,0.25*canvas.height,canvas.width/2,canvas.height/2);
+        };
+        bImg.src = b;
         
         c.fillStyle = a;
         c.fillRect(0.125*canvas.width,0.25*canvas.height,canvas.width/8,canvas.height/3);
@@ -78,12 +96,14 @@ window.onload = function() {
         c.fillRect(0.5*canvas.width+canvas.width/20,0.75*canvas.height,canvas.width/5,canvas.height/4);
         
         document.body.appendChild(canvas);
+        
+        return new Character(canvas.toDataURL("image/png"), elem, name)
     }
     function init() {
         //window.pSrc = "Graphics/kit_from_firefox.png";
         window.pSrc = "Graphics/fox.png";
-        window.char = new Character(pSrc, "fire", "Derp");
-        genChar(0,1,2,3,"water","Piplup");
+        //window.char = new Character(pSrc, "fire", "Derp");
+        window.char = genChar(0,1,2,3,"water","Piplup");
     }
     init();
 };
